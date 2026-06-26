f4d7e35257f15679f026a8f70b1293ae8461815e
Denis Yunyaev <music_is_my_extazy@mail.ru>
2026-06-26 21:57:11 +0300

    feat(app): add global styles, reset and CSS variables

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  sassOptions: {
+    additionalData: `
+      @use "@/src/app/styles/helpers/functions" as *;
+      @use "@/src/app/styles/helpers/media" as *;
+      @use "@/src/app/styles/helpers/mixins" as *;
+      @use "@/src/app/styles/reset" as *;
+    `,
+  },
 };
 
 export default nextConfig;
