f6ae71da7adb241d467ecee5d4c4fc6072193ba0
Denis Yunyaev <music_is_my_extazy@mail.ru>
2026-07-05 22:16:34 +0300

    feat(shared): configure NextAuth credentials and session cookies

--- src/shared/config/auth.ts
+++ src/shared/config/auth.ts
@@ -1,7 +1,20 @@
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
+import Credentials from 'next-auth/providers/credentials';
 import { PrismaAdapter } from '@auth/prisma-adapter';
+import bcrypt from 'bcryptjs';
 import { prisma, UserRole } from '@/src/shared/api';
+import { signInSchema } from '@/src/shared/lib';
+
+export const SESSION_COOKIE_MAX_AGE = {
+  MONTH: 30 * 24 * 60 * 60, // 30 days
+  HOUR: 60 * 60, // 1 hour
+} as const;
+
+export const SESSION_COOKIE_NAME =
+  process.env.NODE_ENV === 'production'
+    ? '__Secure-authjs.session-token'
+    : 'authjs.session-token';
 
 const baseAdapter = PrismaAdapter(prisma);
 
@@ -20,8 +33,61 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       });
     },
   },
-  session: { strategy: 'jwt' }, // JWT – for Credentials
-  providers: [GitHub],
+  providers: [
+    GitHub,
+
+    Credentials({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'Email', type: 'email' },
+        password: { label: 'Password', type: 'password' },
+        rememberMe: { label: 'Remember Me', type: 'text' },
+      },
+      async authorize(credentials) {
+        const preparedCredentials = {
+          ...credentials,
+          rememberMe:
+            credentials?.rememberMe === 'true' ||
+            credentials?.rememberMe === 'on',
+        };
+
+        const validated = signInSchema.safeParse(preparedCredentials);
+        if (!validated.success) return null;
+
+        const { email, password } = validated.data;
+
+        const user = await prisma.user.findUnique({
+          where: { email: email as string },
+        });
+
+        if (!user || !user.password) return null;
+
+        const isValid = await bcrypt.compare(password as string, user.password);
+        if (!isValid) return null;
+
+        return {
+          id: user.id,
+          name: user.username,
+          email: user.email,
+          phone: user.phone,
+          avatar: user.avatar,
+          role: user.role,
+        };
+      },
+    }),
+  ],
+  session: { strategy: 'jwt', maxAge: SESSION_COOKIE_MAX_AGE.HOUR }, // 'jwt' – for Credentials
+  cookies: {
+    sessionToken: {
+      name: SESSION_COOKIE_NAME,
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: process.env.NODE_ENV === 'production',
+      },
+    },
+  },
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
@@ -41,6 +107,6 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     },
   },
   pages: {
-    signIn: '/auth',
+    signIn: '/auth?mode=signin',
   },
 });
