4a311f9140ed739d0f76a3dafae08c9d25b8970f
Denis Yunyaev <music_is_my_extazy@mail.ru>
2026-07-08 11:17:13 +0300

    feat(features): add signin and register forms to auth modal

--- src/features/auth/ui/auth-modal/auth-modal.tsx
+++ src/features/auth/ui/auth-modal/auth-modal.tsx
@@ -1,37 +1,43 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { Button, Modal } from '@/src/shared/ui/client';
 import { cn } from '@/src/shared/lib';
+import { Button, Modal } from '@/src/shared/ui/client';
 import { useAuthMode } from '../../model/use-auth-mode';
 import { OAuthButtons } from '../o-auth-buttons/o-auth-buttons';
+import { RegisterForm } from '../register-form/register-form';
+import { SignInForm } from '../sign-in-form/sign-in-form';
 import s from './auth-modal.module.scss';
 
 export function AuthModal() {
   const router = useRouter();
   const { mode, switchMode } = useAuthMode();
+  const isSignIn = mode === 'signin';
 
   return (
     <Modal
       onClose={() => router.back()}
       aria-labelledby='auth-title'
       closeButtonLabel='Закрыть окно'
-      title={mode === 'signin' ? 'Вход' : 'Регистрация'}
+      title={isSignIn ? 'Вход' : 'Регистрация'}
     >
-      {mode === 'signin' ? (
-        <div>
-          Вход
-          <OAuthButtons />
-        </div>
+      {isSignIn ? (
+        <>
+          <SignInForm />
+          <div className={s.oauthWrapper}>
+            <p>Или войдите через соцсети:</p>
+            <OAuthButtons />
+          </div>
+        </>
       ) : (
-        <div>Регистрация</div>
+        <RegisterForm />
       )}
 
       <Button
         onClick={switchMode}
         className={cn(s.switchBtn)}
       >
-        {mode === 'signin' ? 'Зарегистрироваться' : 'У меня уже есть аккаунт'}
+        {isSignIn ? 'Зарегистрироваться' : 'У меня уже есть аккаунт'}
       </Button>
     </Modal>
   );
