3f3f3f35725fd12ad4a723f33b7ffd9cd437358b
Denis Yunyaev <music_is_my_extazy@mail.ru>
2026-07-08 23:24:46 +0300

    fix(ci): add missing env vars and fix Suspense error on profile page

--- app/(main)/profile/general/page.tsx
+++ app/(main)/profile/general/page.tsx
@@ -1,29 +1,10 @@
-import { SignOutButton } from '@/src/features/auth';
-import { getCurrentUser } from '@/src/entities/user/server';
+import { Suspense } from 'react';
+import { TemporaryUserProfile } from './_temporary-user-profile';
 
-export default async function ProfileGeneralPage() {
-  const user = await getCurrentUser();
-
-  if (!user) {
-    return <div>Загрузка или доступ запрещен...</div>;
-  }
-
+export default function ProfileGeneralPage() {
   return (
-    <div>
-      Общие сведения:
-      <div>email: {user?.email}</div>
-      <div>role: {user?.role}</div>
-      <div>name: {user?.name}</div>
-      <div>
-        {user?.avatar && (
-          <img
-            width={80}
-            src={user.avatar}
-            alt='qwe'
-          />
-        )}
-      </div>
-      <SignOutButton />
-    </div>
+    <Suspense fallback={<div>Загрузка профиля...</div>}>
+      <TemporaryUserProfile />
+    </Suspense>
   );
 }
